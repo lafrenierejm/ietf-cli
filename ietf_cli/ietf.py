8befb2b918e84a575e01652ae802c84a7c596cbf
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-05-24 18:48:04 -0500

    argparse: Require a subcommand

--- ietf_cli/ietf.py
+++ ietf_cli/ietf.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 import argparse
 
-cli_parser = argparse.ArgumentParser()
-cli_subparsers = cli_parser.add_subparsers(help='sub-command help')
 
-# Create the parser for the `mirror` command
-cli_parser_mirror = cli_subparsers.add_parser('mirror',
-                                              help='update a local mirror')
-cli_parser_mirror.add_argument('-d', '--dir',
-                               type=str,
-                               nargs=1,
-                               default=None,
-                               help='directory to download to')
-cli_parser_mirror.add_argument('-t', '--type',
-                               type=str,
-                               nargs='+',
-                               choices=['draft',
-                                        'iana',
-                                        'iesg',
-                                        'charter',
-                                        'status',
-                                        'rfc'],
-                               default='everything',
-                               help='type(s) of files to download')
+def main():
+    cli_parser = argparse.ArgumentParser()
+    cli_subparsers = cli_parser.add_subparsers(dest='subcommand',
+                                               help='subcommand help')
+    cli_subparsers.required = True  # Require a subcommand
 
-cli_args = cli_parser.parse_args()
+    # Create the parser for the `mirror` command
+    cli_parser_mirror = cli_subparsers.add_parser('mirror',
+                                                  help='update a local mirror')
+    cli_parser_mirror.add_argument('-d', '--dir',
+                                   type=str,
+                                   nargs=1,
+                                   default=None,
+                                   help='directory to download to')
+    cli_parser_mirror.add_argument('-t', '--type',
+                                   type=str,
+                                   nargs='+',
+                                   choices=['draft',
+                                            'iana',
+                                            'iesg',
+                                            'charter',
+                                            'status',
+                                            'rfc'],
+                                   default='everything',
+                                   help='type(s) of files to download')
+
+    cli_args = cli_parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
