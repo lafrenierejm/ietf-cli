0f4aba9c8de27f3e4999959c88415e441621c628
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-05-30 21:00:27 -0500

    mirror: Add rsync commands

--- ietf_cli/ietf.py
+++ ietf_cli/ietf.py
@@ -3,6 +3,7 @@ import argparse
 import os
 
 from xdg import BaseDirectory
+from subprocess import Popen
 
 
 def mirror(args):
@@ -26,14 +27,50 @@ def mirror(args):
 
     # Set the top-level mirror directory
     if args.dir is not None:
-        top_dir = args.dir[0]
+        top_dir = os.path.expandvars(os.path.expanduser(args.dir[0]))
     else:
         top_dir = BaseDirectory.save_data_path('ietf-cli')
+    top_dir = os.path.abspath(top_dir)
     # Attempt to create directory
     try:
-        os.mkdir(top_dir)
+        os.makedirs(top_dir)
     except FileExistsError:
         pass  # Directory already exists
+
+    # Run rsync with no document type passed as an argument
+    if args.type is None:
+        # Generate a dict of command arrays from the above dicts
+        commands = {}
+        for doc_type, uri in uris.items():
+            command = ['rsync', '-az', '--delete-during']
+            for exclude in excludes[doc_type]:
+                command.append("--exclude='{}'".format(exclude))
+            command.append('{}'.format(uri))
+            command.append('{}'.format(top_dir + '/' + doc_type))
+            print(command)
+            commands[doc_type] = command
+        processes = []
+        for doc_type, command in commands.items():
+            # Create subdirectory for the document type
+            try:
+                os.makedirs(top_dir + '/' + doc_type)
+            except FileExistsError:  # Directory already exists
+                pass
+            # Start the rsync process and add its PID to processes
+            process = Popen(command)
+            processes.append(process)
+        # Wait for each rsync process to complete
+        exitcodes = [p.wait() for p in processes]
+    # rsync only the specified document type
+    else:
+        command = ['rsync', '-az', '--delete-during']
+        for exclude in excludes[args.type[0]]:
+            command.append("--exclude='{}'".format(exclude))
+        command.append('{}'.format(uris[args.type[0]]))
+        command.append('{}'.format(top_dir))
+        print(command)
+        process = Popen(command)
+        process.wait()
 
 
 def main():
