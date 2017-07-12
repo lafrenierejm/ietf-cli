1ed5b18bb9527bd20278ef311f64c0989d75afe6
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-12 13:59:38 -0500

    [ietf.xml.rfc] Cleanup imports

--- ietf/xml/rfc.py
+++ ietf/xml/rfc.py
@@ -1,32 +1,9 @@
-from ..sql.rfc import Abstract, Author, FileFormat, IsAlso, Keyword,\
-    ObsoletedBy, Obsoletes, Rfc, SeeAlso, UpdatedBy, Updates
-from .enum import DocumentType
-from .parse import findall,\
-    find_abstract,\
-    find_area,\
-    find_author,\
-    find_current_status,\
-    find_date,\
-    find_doc_id,\
-    find_doi,\
-    find_draft,\
-    find_errata_url,\
-    find_format,\
-    find_is_also,\
-    find_keywords,\
-    find_notes,\
-    find_obsoleted_by,\
-    find_obsoletes,\
-    find_publication_status,\
-    find_see_also,\
-    find_stream,\
-    find_title,\
-    find_updated_by,\
-    find_updates,\
-    find_wg_acronym
-
 import sqlalchemy.orm
 import xml.etree.ElementTree
+from ietf.sql.rfc import (Abstract, Author, FileFormat, IsAlso, Keyword,
+                          ObsoletedBy, Obsoletes, Rfc, SeeAlso, UpdatedBy,
+                          Updates)
+import ietf.xml.parse as parse
 
 
 def _add_keyword(session: sqlalchemy.orm.session.Session,
@@ -44,30 +21,30 @@ def add_all(session: sqlalchemy.orm.session.Session,
             root: xml.etree.ElementTree.Element):
     """Add all RFC entries from XML `root` to sqlalchemy `session`."""
 
-    entries = findall(root, 'rfc-entry')
+    entries = parse.findall(root, 'rfc-entry')
     for entry in entries:
-        doc_id = find_doc_id(entry)
-        title = find_title(entry)
-        authors = find_author(entry)
-        year, month, day = find_date(entry)
-        formats = find_format(entry)
-        keywords = find_keywords(entry)
-        abstract_pars = find_abstract(entry)
-        draft = find_draft(entry)
-        notes = find_notes(entry)
-        obsoletes = find_obsoletes(entry)
-        obsoleted_by = find_obsoleted_by(entry)
-        updates = find_updates(entry)
-        updated_by = find_updated_by(entry)
-        is_also = find_is_also(entry)
-        see_also = find_see_also(entry)
-        cur_status = find_current_status(entry)
-        pub_status = find_publication_status(entry)
-        stream = find_stream(entry)
-        area = find_area(entry)
-        wg = find_wg_acronym(entry)
-        errata = find_errata_url(entry)
-        doi = find_doi(entry)
+        doc_id = parse.find_doc_id(entry)
+        title = parse.find_title(entry)
+        authors = parse.find_author(entry)
+        year, month, day = parse.find_date(entry)
+        formats = parse.find_format(entry)
+        keywords = parse.find_keywords(entry)
+        abstract_pars = parse.find_abstract(entry)
+        draft = parse.find_draft(entry)
+        notes = parse.find_notes(entry)
+        obsoletes = parse.find_obsoletes(entry)
+        obsoleted_by = parse.find_obsoleted_by(entry)
+        updates = parse.find_updates(entry)
+        updated_by = parse.find_updated_by(entry)
+        is_also = parse.find_is_also(entry)
+        see_also = parse.find_see_also(entry)
+        cur_status = parse.find_current_status(entry)
+        pub_status = parse.find_publication_status(entry)
+        stream = parse.find_stream(entry)
+        area = parse.find_area(entry)
+        wg = parse.find_wg_acronym(entry)
+        errata = parse.find_errata_url(entry)
+        doi = parse.find_doi(entry)
 
         rfc = Rfc(
             # Create the Rfc object with its single-column values set
@@ -110,7 +87,8 @@ def add_all(session: sqlalchemy.orm.session.Session,
         for doc in obsoleted_by:
             # Add obsoleted_by to rfc
             doc_type, doc_id = doc
-            rfc.obsoleted_by.append(ObsoletedBy(doc_id=doc_id, doc_type=doc_type))
+            rfc.obsoleted_by.append(ObsoletedBy(doc_id=doc_id,
+                                                doc_type=doc_type))
         for doc in updates:
             # Add updates to rfc
             doc_type, doc_id = doc
