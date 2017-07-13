e851af4e922b568c415a07b1934ee42e36e305c5
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-13 16:38:10 -0500

    [ietf.sql.rfc] Fully represent Rfc with a string

--- ietf/sql/rfc.py
+++ ietf/sql/rfc.py
@@ -16,7 +16,11 @@ class Abstract(Base):
     rfc = relationship('Rfc', back_populates='abstract')
 
     def __repr__(self):
-        return "<Abstract(first_10_chars='%s')>" % self.par[0:10]
+        # 79 - (18 + 3) - 3 = 55
+        # 79 to adhere to 80 column width
+        # -(18 + 3) from Rfc.__repr__.fmt
+        # -3 from ellipses
+        return "{}...".format(self.par[:55])
 
 
 class Author(Base):
@@ -32,7 +36,15 @@ class Author(Base):
     rfc = relationship('Rfc', back_populates='authors')
 
     def __repr__(self):
-        return "<Author(name='%s')>" % self.name
+        """String representation of object."""
+        representation = self.name
+        if self.title:
+            representation += ', ' + self.title
+        if self.organization:
+            representation += ', ' + self.organization
+        if self.org_abbrev:
+            representation += ', ' + self.org_abbrev
+        return representation
 
 
 class FileFormat(Base):
@@ -47,9 +59,11 @@ class FileFormat(Base):
     rfc = relationship('Rfc', back_populates='formats')
 
     def __repr__(self):
-        return "<Format(filetype='%s', char_count='%d', page_count='%d',"\
-            "rfc_id='%s')>" % (self.filetype.value, self.char_count,
-                               self.page_count, self.rfc_id)
+        representation = "filetype={}, char count={}".\
+            format(self.filetype.value, self.char_count)
+        if self.page_count:
+            representation += ", page count=" + str(self.page_count)
+        return representation
 
 
 class IsAlso(Base):
@@ -63,8 +77,7 @@ class IsAlso(Base):
     rfc = relationship('Rfc', back_populates='is_also')
 
     def __repr__(self):
-        return "<IsAlso(id='%s %d', host='%d')>" % (self.doc_type.value,
-                                                    self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 rfc_keyword = Table(
@@ -90,6 +103,9 @@ class Keyword(Base):
     def __init__(self, word):
         self.word = word
 
+    def __repr__(self):
+        return self.word
+
 
 class ObsoletedBy(Base):
     __tablename__ = 'obsoleted_by'
@@ -102,8 +118,7 @@ class ObsoletedBy(Base):
     rfc = relationship('Rfc', back_populates='obsoleted_by')
 
     def __repr__(self):
-        return "<ObsoletedBy(id='%s %d', obsoleted='%d')>"\
-            % (self.doc_type.value, self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 class Obsoletes(Base):
@@ -117,8 +132,7 @@ class Obsoletes(Base):
     rfc = relationship('Rfc', back_populates='obsoletes')
 
     def __repr__(self):
-        return "<Obsoletes(id='%s %d', obsoleting id='%d')>"\
-            % (self.doc_type.value, self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 class SeeAlso(Base):
@@ -132,8 +146,7 @@ class SeeAlso(Base):
     rfc = relationship('Rfc', back_populates='see_also')
 
     def __repr__(self):
-        return "<SeeAlso(id='%s %d', host='%d')>"\
-            % (self.doc_type.value, self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 class Stream(Base):
@@ -163,8 +176,7 @@ class UpdatedBy(Base):
     rfc = relationship('Rfc', back_populates='updated_by')
 
     def __repr__(self):
-        return "<UpdatedBy(id='%s %d', updated='%d')>"\
-            % (self.doc_type.value, self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 class Updates(Base):
@@ -178,8 +190,7 @@ class Updates(Base):
     rfc = relationship('Rfc', back_populates='updates')
 
     def __repr__(self):
-        return "<Updates(id='%s %d', updated by='%d')>"\
-            % (self.doc_type.value, self.id, self.rfc_id)
+        return "{} {}".format(self.doc_type.value, self.doc_id)
 
 
 class Rfc(Base):
@@ -221,5 +232,128 @@ class Rfc(Base):
     doi = Column(String)
 
     def __repr__(self):
-        return "RfcEntry(id=rfc{}, title={}".format(
-            self.id, self.title)
+        """String representation of Rfc instances."""
+
+        # First string is left-aligned with a width of 18
+        fmt = "{:<18} : {}"
+
+        id_str = fmt.format('RFC', "{:0>4}".format(self.id))
+        title_str = fmt.format('Title', self.title)
+
+        author_strs = []
+        for row in self.authors:
+            author_strs.append(fmt.format('Author', row.__repr__()))
+
+        if self.date_day:
+            date_str = fmt.format(
+                'Date',
+                "{:0>4}-{:0>2}-{:0>2}".format(self.date_year,
+                                              self.date_month,
+                                              self.date_day)
+            )
+        else:
+            date_str = fmt.format(
+                'Date',
+                "{:0>4}-{:0>2}".format(self.date_year,
+                                       self.date_month)
+            )
+
+        format_strs = []
+        if self.formats:
+            for row in self.formats:
+                format_strs.append(fmt.format('Format', row.__repr__()))
+
+        keyword_strs = []
+        if self.keywords:
+            for row in self.keywords:
+                keyword_strs.append(fmt.format('Keyword', row.__repr__()))
+
+        abstract_strs = []
+        if self.abstract:
+            for row in self.abstract:
+                abstract_strs.append(fmt.format('Abstract', row.__repr__()))
+
+        notes_strs = []
+        if self.notes:
+            for row in self.notes:
+                notes_strs.append(fmt.format('Note', row.__repr__()))
+
+        obsoletes_strs = []
+        if self.obsoletes:
+            for row in self.obsoletes:
+                obsoletes_strs.append(fmt.format('Obsoletes', row.__repr__()))
+
+        obsoleted_by_strs = []
+        if self.obsoleted_by:
+            for row in self.obsoleted_by:
+                obsoleted_by_strs.append(fmt.format('Obsoleted By',
+                                                    row.__repr__()))
+
+        updates_strs = []
+        if self.updates:
+            for row in self.updates:
+                updates_strs.append(fmt.format('Updates', row.__repr__()))
+
+        updated_by_strs = []
+        if self.updated_by:
+            for row in self.updated_by:
+                updated_by_strs.append(fmt.format('Updated By',
+                                                  row.__repr__()))
+
+        is_also_strs = []
+        if self.is_also:
+            for row in self.is_also:
+                is_also_strs.append(fmt.format('Is Also', row.__repr__()))
+
+        see_also_strs = []
+        if self.see_also:
+            for row in self.see_also:
+                see_also_strs.append(fmt.format('See Also', row.__repr__()))
+
+        current_status_str = fmt.format('Current Status',
+                                        self.current_status.value)
+        publication_status_str = fmt.format('Publication Status',
+                                            self.publication_status.value)
+
+        stream_strs = []
+        if self.stream:
+            for row in self.stream:
+                stream_strs.append(fmt.format('Stream', row.__repr__()))
+
+        if self.area:
+            area_str = fmt.format('Area', self.area)
+        if self.wg_acronym:
+            wg_str = fmt.format('WG Acronym', self.wg_acronym)
+        if self.errata_url:
+            errata_str = fmt.format('Errata URL', self.errata_url)
+        if self.doi:
+            doi_str = fmt.format('DOI', self.doi)
+
+        # Assemble a list of the defined strings
+        list_repr = [id_str, title_str]
+        list_repr.extend(author_strs)
+        list_repr.append(date_str)
+        list_repr.extend(format_strs)
+        list_repr.extend(keyword_strs)
+        list_repr.extend(abstract_strs)
+        list_repr.extend(notes_strs)
+        list_repr.extend(obsoletes_strs)
+        list_repr.extend(obsoleted_by_strs)
+        list_repr.extend(updates_strs)
+        list_repr.extend(updated_by_strs)
+        list_repr.extend(is_also_strs)
+        list_repr.extend(see_also_strs)
+        list_repr.append(current_status_str)
+        list_repr.append(publication_status_str)
+        list_repr.extend(stream_strs)
+        if 'area_str' in vars():
+            list_repr.append(area_str)
+        if 'wg_str' in vars():
+            list_repr.append(wg_str)
+        if 'errata_str' in vars():
+            list_repr.append(errata_str)
+        if 'doi_str' in vars():
+            list_repr.append(doi_str)
+
+        # Return a string of list_repr's elements joined using newlines
+        return '\n'.join(list_repr)
