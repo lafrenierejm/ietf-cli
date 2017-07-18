32ad65bed15035f4c54709a4a945a7c0e196721a
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-18 14:14:38 -0500

    Add ietf.utility.query

---
+++ ietf/utility/query.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from ietf.utility.environment import get_db_path
+from ietf.sql.base import Base
+from ietf.sql.rfc import Rfc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def get_db_session():
+    """Return a DB session."""
+    db_path = get_db_path()
+    engine = create_engine("sqlite:///{}".format(db_path))
+    Base.metadata.create_all(engine, checkfirst=True)
+    session = sessionmaker(bind=engine)()
+    return session
+
+
+def query_rfc_by_id(session, number):
+    row = session.query(Rfc).\
+                  filter(Rfc.id == number).\
+                  one_or_none()
+    return row
