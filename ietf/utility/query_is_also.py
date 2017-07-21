54abf9ba9b203def05018b59916510da4471fbfb
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-21 14:49:28 -0500

    [ietf.cmd.bcp] Add `bcp` subcommand

--- ietf/utility/query_is_also.py
+++ ietf/utility/query_is_also.py
@@ -1,6 +1,23 @@
-from ietf.xml.enum import DocumentType
+from ietf.sql.bcp import Bcp
+from ietf.sql.rfc import IsAlso, Rfc
 from ietf.utility.query_doc import (query_bcp, query_fyi, query_rfc,
                                     query_std,)
+from ietf.xml.enum import DocumentType
+
+
+def query_bcp_is_also(Session, number):
+    docs = []  # List of docs to return
+    rfcs = Session.query(Rfc).join(IsAlso).\
+        filter(IsAlso.doc_type == DocumentType.BCP).\
+        filter(IsAlso.doc_id == number).\
+        all()  # Returns a list
+    docs.extend(rfcs)  # Add rfcs to list
+    for rfc in rfcs:
+        rfc_aliases = query_rfc_is_also(Session, rfc.id)
+        for doc in rfc_aliases:
+            if (not isinstance(doc, Bcp)) or (doc.id != number):
+                docs.append(doc)
+    return docs
 
 
 def query_rfc_is_also(Session, number):
