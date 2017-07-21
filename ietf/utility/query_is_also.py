86258f714ecd3649873f744f60f2d796d304d4c1
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-21 14:51:19 -0500

    Fix indentation in `query_rfc_is_also()`
    The comparisons on document type now run per-document rather than being
    outside of the loop where document information was extracted.

--- ietf/utility/query_is_also.py
+++ ietf/utility/query_is_also.py
@@ -30,14 +30,14 @@ def query_rfc_is_also(Session, number):
         for alias in rfc.is_also:
             alias_type = alias.doc_type
             alias_id = alias.doc_id
-        if alias_type is DocumentType.RFC:
-            aliases.append(query_rfc(Session, alias_id))
-        elif alias_type is DocumentType.STD:
-            aliases.append(query_std(Session, alias_id))
-        elif alias_type is DocumentType.BCP:
-            aliases.append(query_bcp(Session, alias_id))
-        elif alias_type is DocumentType.FYI:
-            aliases.append(query_fyi(Session, alias_id))
-        else:
-            aliases.append(alias)
+            if alias_type is DocumentType.RFC:
+                aliases.append(query_rfc(Session, alias_id))
+            elif alias_type is DocumentType.STD:
+                aliases.append(query_std(Session, alias_id))
+            elif alias_type is DocumentType.BCP:
+                aliases.append(query_bcp(Session, alias_id))
+            elif alias_type is DocumentType.FYI:
+                aliases.append(query_fyi(Session, alias_id))
+            else:
+                aliases.append(alias)
     return aliases
